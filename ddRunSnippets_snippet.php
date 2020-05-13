b368e8eef46f726466c1f546f737c70775434214
Ilyas Ronef <ilyas@DivanDesign.ru>
2020-05-13 03:45:09 +0300

    * Minor changes.

--- ddRunSnippets_snippet.php
+++ ddRunSnippets_snippet.php
@@ -10,10 +10,17 @@
  * @copyright 2011–2016 DD Group {@link https://DivanDesign.biz }
  */
 
-//Подключаем modx.ddTools
-require_once $modx->config['base_path'].'assets/libs/ddTools/modx.ddtools.class.php';
+//Include (MODX)EvolutionCMS.libraries.ddTools
+require_once(
+	$modx->getConfig('base_path') .
+	'assets/libs/ddTools/modx.ddtools.class.php'
+);
 
-$resultPrefix = isset($resultPrefix) ? $resultPrefix : 'ddresult';
+$resultPrefix =
+	isset($resultPrefix) ?
+	$resultPrefix :
+	'ddresult'
+;
 
 $resArr = [];
 $result = '';
@@ -21,46 +28,67 @@ $result = '';
 //Все параметры сниппета (скопируем, чтобы в глобал не срать)
 $ddParams = $params;
 
-$i = isset($ddParams['snipName']) ? '' : 0;
+$i =
+	isset($ddParams['snipName']) ?
+	'' :
+	0
+;
 
-while(isset($ddParams['snipName'.$i])){
+while(isset($ddParams['snipName' . $i])){
 	$snipParamsArr = [];
 	
 	//Если имена параметров и их значения заданы
 	if (
-		isset($ddParams['snipParams'.$i]) &&
-		isset($ddParams['snipValues'.$i])
+		isset($ddParams['snipParams' . $i]) &&
+		isset($ddParams['snipValues' . $i])
 	){
 		//Подставляем в названия параметров и в значения (смотря куда надо) результат работы предыдущего сниппета
-		$ddParams['snipParams'.$i] = ddTools::parseText([
-			'text' => $ddParams['snipParams'.$i],
+		$ddParams['snipParams' . $i] = \ddTools::parseText([
+			'text' => $ddParams['snipParams' . $i],
 			'data' => $resArr,
 			'placeholderPrefix' => '+',
 			'placeholderSuffix' => '+'
 		]);
-		$ddParams['snipValues'.$i] = ddTools::parseText([
-			'text' => $ddParams['snipValues'.$i],
+		$ddParams['snipValues' . $i] = \ddTools::parseText([
+			'text' => $ddParams['snipValues' . $i],
 			'data' => $resArr,
 			'placeholderPrefix' => '+',
 			'placeholderSuffix' => '+'
 		]);
 		
 		//Разбиваем на массивы
-		$ddParams['snipParams'.$i] = explode(',', $ddParams['snipParams'.$i]);
-		$ddParams['snipValues'.$i] = explode('##', $ddParams['snipValues'.$i]);
+		$ddParams['snipParams' . $i] = explode(
+			',',
+			$ddParams['snipParams' . $i]
+		);
+		$ddParams['snipValues' . $i] = explode(
+			'##',
+			$ddParams['snipValues' . $i]
+		);
 		
 		//Если размеры массивов параметров и значений совпадают
-		if (count($ddParams['snipParams'.$i]) == count($ddParams['snipValues'.$i])){
-			$snipParamsArr = array_combine($ddParams['snipParams'.$i], $ddParams['snipValues'.$i]);
+		if (count($ddParams['snipParams' . $i]) == count($ddParams['snipValues' . $i])){
+			$snipParamsArr = array_combine(
+				$ddParams['snipParams' . $i],
+				$ddParams['snipValues' . $i]
+			);
 		}
 	}
 	
-	$snipAlias = $resultPrefix.$i;
-	$snipName = $ddParams['snipName'.$i];
+	$snipAlias = $resultPrefix . $i;
+	$snipName = $ddParams['snipName' . $i];
 	
 	//Если задан псевдоним результата сниппета
-	if (strpos($snipName, '::') !== false){
-		$temp = explode('::', $snipName);
+	if (
+		strpos(
+			$snipName,
+			'::'
+		) !== false
+	){
+		$temp = explode(
+			'::',
+			$snipName
+		);
 		
 		$snipName = $temp[0];
 		
@@ -71,16 +99,36 @@ while(isset($ddParams['snipName'.$i])){
 	
 	//Если что-то передали
 	if (count($snipParamsArr) > 0){
-		$resArr[$snipAlias] = $modx->runSnippet($snipName, $snipParamsArr);
+		$resArr[$snipAlias] = $modx->runSnippet(
+			$snipName,
+			$snipParamsArr
+		);
 	}else{
 		$resArr[$snipAlias] = $modx->runSnippet($snipName);
 	}
 	
-	if ($i === ''){$i = 0;}else{$i++;}
+	if ($i === ''){
+		$i = 0;
+	}else{
+		$i++;
+	}
 }
 
-$num = isset($num) ? explode(',', $num) : ['last'];
-$glue = isset($glue) ? $glue : '';
+$num =
+	isset($num) ?
+	explode(
+		',',
+		$num
+	) :
+	[
+		'last'
+	]
+;
+$glue =
+	isset($glue) ?
+	$glue :
+	''
+;
 
 //Если задан шаблон для вывода
 if (
@@ -88,30 +136,57 @@ if (
 	$tpl != ''
 ){
 	//Убиваем пустые результаты TODO: Подумать, возможно, сделать параметрально
-	if(!function_exists('ddArrayFilterEmpty')){function ddArrayFilterEmpty($el){return !empty($el);}}
-	$resArr = array_filter($resArr, 'ddArrayFilterEmpty');
+	if(!function_exists('ddArrayFilterEmpty')){
+		function ddArrayFilterEmpty($el){
+			return !empty($el);
+		}
+	}
+	
+	$resArr = array_filter(
+		$resArr,
+		'ddArrayFilterEmpty'
+	);
 	
 	//Если есть хоть один результат
 	if (count($resArr) > 0){
 		//Если есть дополнительные данные
 		if (isset($placeholders)){
 			//Разбиваем их
-			$resArr = array_merge($resArr, ddTools::explodeAssoc($placeholders));
+			$resArr = array_merge(
+				$resArr,
+				\ddTools::explodeAssoc($placeholders)
+			);
 		}
 		
-		$result .= $modx->parseChunk($tpl, $resArr, '[+','+]');
+		$result .= $modx->parseChunk(
+			$tpl,
+			$resArr,
+			'[+',
+			'+]'
+		);
 	}
 //Если шаблон не задан
 }else{
 	//Если надо вывести всё
-	if (in_array('all', $num)){
-		$result .= implode($glue, $resArr);
+	if (
+		in_array(
+			'all',
+			$num
+		)
+	){
+		$result .= implode(
+			$glue,
+			$resArr
+		);
 	}else{
 		//Перебираем массив номеров результатов сниппетов
-		foreach ($num as $n){
+		foreach (
+			$num as
+			$n
+		){
 			//Если передан именно номер сниппета
 			if (is_numeric($n)){
-				$n = $resultPrefix.$n;
+				$n = $resultPrefix . $n;
 			}
 			
 			//Если надо просто последний или такого элемента нет
@@ -134,7 +209,14 @@ if (
 	isset($toPlaceholder) &&
 	$toPlaceholder == '1'
 ){
-	$modx->setPlaceholder(isset($placeholderName) ? $placeholderName : 'ddRunSnippets', $result);
+	$modx->setPlaceholder(
+		(
+			isset($placeholderName) ?
+			$placeholderName :
+			'ddRunSnippets'
+		),
+		$result
+	);
 }else{
 	return $result;
 }
